44f8a892a4533a6935969a8a8e733f6065a6a76c
bakuyy <sophieyang12345@gmail.com>
2024-09-28 16:58:36 -0400

    created landing page layout and added animations to character box

--- frontend/src/components/Characterbox.js
+++ frontend/src/components/Characterbox.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Characterbox = () => {
   return (
-    <div>
-      
+    <div className="flex justify-center items-center h-full px-8 mt-16">
+      <div className="bg-white rounded-lg shadow-lg w-80 h-96 text-center transition-transform duration-300 transform hover:shadow-white hover:scale-102 hover:white  ">
+        hi
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Characterbox
+export default Characterbox;
