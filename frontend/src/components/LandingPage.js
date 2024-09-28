7ff4f61399ae54396c81689149f78fb25311148a
bakuyy <sophieyang12345@gmail.com>
2024-09-28 16:58:36 -0400

    created landing page layout and added animations to character box

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Characterbox from './Characterbox';
 
 const LandingPage = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="flex flex-col h-screen overflow-hidden">
+      <div className='absolute right-0 px-12 py-2 bg-white rounded-xl mt-10 mr-12'>
+        <button>finish</button>
+      </div>
+      <div className="flex space-x-4 hover ml-5 mb-32">
+        <Characterbox />
+        <Characterbox />
+        <Characterbox />
+      </div>
 
-export default LandingPage
+      {/* <div className="flex space-x-4">
+        <div className="flex-grow-4 w-96 text-center rounded-lg bg-white p-16 ml-12">AAPL</div>
+        <div className="flex-grow-1 right-0 text-center rounded-lg bg-white p-16">AAPL</div>
+      </div> */}
+    </div>
+  );
+};
+
+export default LandingPage;
